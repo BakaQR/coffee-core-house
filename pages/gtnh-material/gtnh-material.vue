a147cf5392be2fef81a51ee5b33fc7436b80151d
BakaQR <bakaqm@gmail.com>
2026-06-12 16:50:13 +0800

    refactor: 移除不必要的标题和样式属性

--- pages/gtnh-material/gtnh-material.vue
+++ pages/gtnh-material/gtnh-material.vue
@@ -1,6 +1,5 @@
 <template>
   <view class="content">
-    <text class="title">GTNH 材料计算器</text>
     <!-- 在这里添加组件的内容 -->
   </view>
 </template>
@@ -14,7 +13,6 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 }
 
 .title {
